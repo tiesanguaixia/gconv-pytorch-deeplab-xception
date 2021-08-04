0f0d9265729b5dfbab57745068385590b9aa9eb6
tiesanguaixia <45888098+tiesanguaixia@users.noreply.github.com>
2021-08-04 21:39:08 +0800

    Update train_voc.sh

--- train_voc.sh
+++ train_voc.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py --backbone resnet --lr 0.007 --workers 4 --use-sbd True --epochs 50 --batch-size 16 --gpu-ids 0,1,2,3 --checkname deeplab-resnet --eval-interval 1 --dataset pascal
+CUDA_VISIBLE_DEVICES=0 python content/gconv-pytorch-deeplab-xception/train.py --backbone resnet --lr 0.007 --workers 1 --use-sbd 1 --epochs 50 --batch-size 16 --gpu-ids 0 --checkname deeplab-resnet --eval-interval 1 --dataset pascal
