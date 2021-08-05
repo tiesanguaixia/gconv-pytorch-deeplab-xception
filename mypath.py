009fad814b13622fd2027330df2f00d9a31d0594
tiesanguaixia <45888098+tiesanguaixia@users.noreply.github.com>
2021-08-06 07:05:43 +0800

    Update mypath.py

--- mypath.py
+++ mypath.py
@@ -2,9 +2,9 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            return '/content/VOCdevkit/VOC2012'  # folder that contains VOCdevkit/.
+            return '/VOCdevkit/VOC2012'  # folder that contains VOCdevkit/.
         elif dataset == 'sbd':
-            return '/content/benchmark_RELEASE'  # folder that contains dataset/.
+            return '/benchmark_RELEASE'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
             return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
         elif dataset == 'coco':
